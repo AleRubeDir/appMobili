60b96fa8a3a70c01db478ad416d9b838d629ec73
AleRubeDir <20029064@studenti.uniupo.it>
2021-04-29 19:15:16 +0200

    update mappa

--- app/src/main/java/it/uniupo/progetto/ClientMappa.kt
+++ app/src/main/java/it/uniupo/progetto/ClientMappa.kt
@@ -1,7 +1,6 @@
 package it.uniupo.progetto
 
 import android.Manifest
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Color
 import android.location.Address
@@ -9,6 +8,7 @@ import android.location.Geocoder
 import android.location.Location
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -66,10 +66,10 @@ class ClientMappa : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
+        mMap.uiSettings.isMyLocationButtonEnabled = false
+        val mypos = findViewById<ImageButton>(R.id.find)
         val permission = ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
-
         if (permission == PackageManager.PERMISSION_GRANTED) {
             mMap.isMyLocationEnabled = true
         } else {
@@ -77,56 +77,58 @@ class ClientMappa : AppCompatActivity(), OnMapReadyCallback {
                     Manifest.permission.ACCESS_FINE_LOCATION,
                     LOCATION_REQUEST_CODE)
         }
-        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        lateinit var mylocation : Location
-        fusedLocationClient.lastLocation
+
+        var geocodeMatches: List<Address>? = null
+        try{
+            geocodeMatches = Geocoder(this).getFromLocationName("San Salvatore ,AL", 1)
+        }catch (e: IOException){
+            e.printStackTrace()
+        }
+        var zoomLevel = 11.0f //This goes up to 21
+        if(geocodeMatches!=null){
+            for(mat in geocodeMatches) {
+                val market = LatLng(mat.latitude, mat.longitude)
+                mMap.addMarker(MarkerOptions().position(market).title("MiniMarket"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(market, zoomLevel))
+                val circle = mMap.addCircle(CircleOptions()
+                        .center(LatLng(mat.latitude, mat.longitude))
+                        .radius(10000.0)
+                        .strokeColor(Color.RED)
+                        .fillColor(0x000C1FFFF)
+
+               )
+                mypos.setOnClickListener{
+                val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+                fusedLocationClient.lastLocation
                 .addOnSuccessListener { location: Location? ->
                     // Got last known location. In some rare situations this can be null.
                     if (location != null) {
-                        mylocation = location
+                        zoomLevel=16.0f
+                        val cliente = LatLng(location.latitude, location.longitude)
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cliente, zoomLevel))
                         Toast.makeText(this, "Lat " + location.latitude + " and long: " + location.longitude, Toast.LENGTH_LONG).show()
-                        Log.d("maps", "mylocation vale $mylocation")
-                        // do something, save it perhaps?
-                        var geocodeMatches: List<Address>? = null
-                        try{
-                            geocodeMatches = Geocoder(this).getFromLocationName("via Circonvallazione Ovest 35 Valenza", 1)
-                        }catch (e: IOException){
-                            e.printStackTrace()
-                        }
-                        if(geocodeMatches!=null){
-                            for(mat in geocodeMatches) {
-                                val market = LatLng(mat.latitude, mat.longitude)
-
-                                mMap.addMarker(MarkerOptions().position(market).title("MiniMarket"))
-                                mMap.moveCamera(CameraUpdateFactory.newLatLng(market))
-                                val circle = mMap.addCircle(CircleOptions()
-                                        .center(LatLng(mat.latitude, mat.longitude))
-                                        .radius(10000.0)
-                                        .strokeColor(Color.RED)
-                                        .fillColor(R.color.mapCircle))
-                                if(isInside(mylocation.latitude,mylocation.longitude,circle)==1){
+                                if(isInside(location.latitude, location.longitude, circle)==1){
                                     //dentro
-                                    startActivity(Intent(this, HomeActivity::class.java))
+                                    // startActivity(Intent(this, HomeActivity::class.java))
                                 }else{
                                     //fuori
                                 }
-
-                                Log.d("maps", "mylocation vale $mylocation minimarket vale $market e circle vale ${circle}")
-
-
+                                Log.d("maps", "mylocation vale $location minimarket vale $market e circle vale ${circle}")
                             }
                         }
                     }
                 }
+
+        }
     }
-    private fun isInside(lat : Double, long : Double, circle : Circle):Int{
+    private fun isInside(lat: Double, long: Double, circle: Circle):Int{
         var distance = FloatArray(2)
-        Location.distanceBetween(lat, long,circle.center.latitude,circle.center.longitude,distance)
+        Location.distanceBetween(lat, long, circle.center.latitude, circle.center.longitude, distance)
         if(distance[0]>circle.radius){
-            Toast.makeText(this,"Oltre 10KM", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Oltre 10KM", Toast.LENGTH_SHORT).show()
             return 1
         }else{
-            Toast.makeText(this,"Entro i 10KM", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Entro i 10KM", Toast.LENGTH_SHORT).show()
             return -1
         }
     }
